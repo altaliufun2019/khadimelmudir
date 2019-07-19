e33c019ce92c2434780086b9fb12ecf494aa4095
sajadrezvani <Rezvani@ce.sharif.edu>
2019-07-19 17:28:59 +0430

    added task update, renewed models

--- models/project.go
+++ models/project.go
@@ -1,11 +1,9 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type Project struct {
-	Name          string             `json:"name"`
-	CreatedDate   primitive.DateTime `json:"created_date"`
-	Description   string             `json:"description"`
-	Collaborators string             `json:"collaborators"`
-	Tasks         []Task             `json:"tasks"`
+	Name          string `json:"name"`
+	CreatedDate   string `json:"created_date"`
+	Description   string `json:"description"`
+	Collaborators string `json:"collaborators"`
+	Tasks         []Task `json:"tasks"`
 }
